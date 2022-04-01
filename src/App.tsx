5144b3949e8d900c78e12c46343aad72b2e4c81f
artinc5233 <artinc5233@gmail.com>
2022-04-01 16:51:26 +0900

    esint 설정, tsconfig 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useLayoutEffect } from 'react';
+import rough from 'roughjs';
+const generator = rough.generator();
 
 function App() {
+  useLayoutEffect(() => {
+    const canvas = document.querySelector('#Vanvas') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+
+    const roughCanvas = rough.canvas(canvas);
+    const rect = generator.rectangle(10, 10, 100, 100);
+    roughCanvas.draw(rect);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas
+        id="Canvas"
+        width={window.innerWidth}
+        height={window.innerHeight}
+      ></canvas>
     </div>
   );
 }
