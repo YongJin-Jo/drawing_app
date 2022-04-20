eb1609a248fcd1db57d09734ad7d2b91b8ca149d
YongJin-Jo <artinc5233@gmail.com>
2022-04-20 23:50:43 +0900

    PositionTarget data 타입 수정,  Drawable, Opset 인터페이스  생성,  SetsDefind 삭제

--- src/type/Canvas/index.ts
+++ src/type/Canvas/index.ts
@@ -1,9 +1,22 @@
-interface PositionTarget {
+export interface PositionTarget {
   startX: number;
   startY: number;
   endX: number;
   endY: number;
   toolType?: string;
+  data?: Drawable;
 }
 
-export type { PositionTarget };
+//drawingTool
+export interface Drawable {
+  shape: string;
+  sets: OpSet[];
+}
+
+//generator
+
+//render
+export interface OpSet {
+  type: string;
+  ops: { op: string; data: number[] }[];
+}
